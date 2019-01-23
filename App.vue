060f943f08a13c442f46a04892e91737a91263f9
Jtaugner <31822824+Jtaugner@users.noreply.github.com>
2019-01-23 20:06:25 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,26 @@
+<template>
+    <view class="container">
+        <text class="text-color-primary">{{message}}</text>
+    </view>
+</template>
+<script>
+    export default {
+        data: function() {
+            return {
+                message: "Hello World"
+            };
+        }
+    };
+</script>
+<style>
+    .container {
+        flex: 1;
+        background-color: white;
+        align-items: center;
+        justify-content: center;
+    }
+    .text-color-primary {
+        color: blue;
+        font-size: 30;
+    }
+</style>
